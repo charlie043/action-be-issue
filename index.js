a4c3f774ef4a3269fc4ee25f5d13f6d73982b108
Hiromasa Uematsu <hiromasa.uematsu@linecorp.com>
2019-12-27 13:37:41 +0900

    test

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    console.log(process.env)
+    const githubToken = core.getInput('myToken');
+    const octokit = new github.GitHub(githubToken);
+
+    const [ GITHUB_USER, GITHUB_REPOS ] = process.env.GITHUB_REPOSITORY
+
+    const commit = await octokit.git.getCommit({
+      owner: GITHUB_USER,
+      repo: GITHUB_REPOS,
+      commit_sha: process.env.GITHUB_SHA
+    })
+    console.log(commit)
   }
   catch (error) {
     core.setFailed(error.message);
