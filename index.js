87f483e5e34114a6e9413547ce23d2c4c256cc69
Hiromasa Uematsu <hiromasa.uematsu@linecorp.com>
2019-12-31 15:02:00 +0900

    test

--- index.js
+++ index.js
@@ -7,59 +7,60 @@ const btoa = require('btoa')
 async function run() {
   try {
     const githubToken = core.getInput('myToken');
-    console.log(githubToken)
     const octokit = new github.GitHub(githubToken);
 
-    const [ GITHUB_USER, GITHUB_REPOS ] = process.env.GITHUB_REPOSITORY.split('/')
+    const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/')
 
     const commit = await octokit.git.getCommit({
-      owner: GITHUB_USER,
-      repo: GITHUB_REPOS,
-      commit_sha: process.env.GITHUB_SHA,
-      mediaType: { format: 'diff' }
+      owner,
+      repo,
+      commit_sha: process.env.GITHUB_SHA
     })
     const commitSha = commit.data.sha
     const parentSha = commit.data.parents[0].sha
+
     const compare = await octokit.repos.compareCommits({
-      owner: GITHUB_USER,
-      repo: GITHUB_REPOS,
+      owner,
+      repo,
       base: parentSha,
       head: commitSha
     })
-    const file = compare.data.files[0]
-    const blob = await octokit.git.getBlob({
-      owner: GITHUB_USER,
-      repo: GITHUB_REPOS,
-      file_sha: file.sha
-    })
-    const raw = atob(blob.data.content)
-    const lines = raw.split('\n')
-    const newLines = []
-    for (let line of lines) {
-      let newLine = ''
-      if (line.match(/- \[i\] .+/)) {
-        const title = line.replace('- [i] ', '')
-        const issue = await octokit.issues.create({
-          owner: GITHUB_USER,
-          repo: GITHUB_REPOS,
-          title
-        })
-        console.log(issue)
-        newLine = `- [#${issue.data.number}](${issue.data.html_url}) ${title}`
-      } else {
-        newLine = line
+
+    const { files } = compare.data
+    for (let file of files) {
+      const blob = await octokit.git.getBlob({
+        owner,
+        repo,
+        file_sha: file.sha
+      })
+      const raw = atob(blob.data.content)
+      const lines = raw.split('\n')
+      const newLines = []
+      for (let line of lines) {
+        let newLine = ''
+        if (line.match(/- \[i\] .+/)) {
+          const title = line.replace('- [i] ', '')
+          const issue = await octokit.issues.create({
+            owner,
+            repo,
+            title
+          })
+          newLine = `- [#${issue.data.number}](${issue.data.html_url}) ${title}`
+        } else {
+          newLine = line
+        }
+        newLines.push(newLine)
       }
-      newLines.push(newLine)
+      const newRaw = newLines.join('\n')
+      await octokit.repos.createOrUpdateFile({
+        owner,
+        repo,
+        path: file.filename,
+        sha: file.sha,
+        message: 'create issues',
+        content: btoa(newRaw)
+      })
     }
-    const newRaw = newLines.join('\n')
-    await octokit.repos.createOrUpdateFile({
-      owner: GITHUB_USER,
-      repo: GITHUB_REPOS,
-      path: file.filename,
-      sha: file.sha,
-      message: 'create issues',
-      content: btoa(newRaw)
-    })
   }
   catch (error) {
     core.setFailed(error.message);
